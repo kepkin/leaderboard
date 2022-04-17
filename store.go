b1fd1e0442063b28f1f762cad18764d56edc7e02
Alexander Nevskiy <kepkin@gmail.com>
2022-04-17 19:52:27 +0300

    Add support for removal key and searching by key

--- store.go
+++ store.go
@@ -19,28 +19,41 @@ flush
 
 type Lesser interface {
 	Less(than Lesser) bool
+	Key() string
 }
 
-type IntLesser int64
+type IntLesser struct {
+	Value int64
+	KeyValue   string
+}
 
 func (s IntLesser) Less(than Lesser) bool {
-	return s < than.(IntLesser)
+	return s.Value < than.(IntLesser).Value
 }
 
+func (s IntLesser) Key() string {
+	return s.KeyValue
+}
+
+
+type OnSplitTrigger func(Lesser, *Node)
 
 type Node struct {
 	Parent *Node
 	Size uint16
 	Data []Lesser
 	Childs []*Node
+
+	OnSplit OnSplitTrigger
 }
 
-func NewNode(size uint16, parent *Node) *Node {
+func NewNode(size uint16, parent *Node, onSplit OnSplitTrigger) *Node {
 	return &Node{
 		Parent: parent,
 		Size: size,
 		Data: make([]Lesser, 0, size),
 		Childs: make([]*Node, size+1),
+		OnSplit: onSplit,
 	}
 }
 
@@ -59,41 +72,89 @@ func (n *Node) Split() *Node {
 
 	if upperNode == nil {
 
-		upperNode = NewNode(n.Size, nil)
+		upperNode = NewNode(n.Size, nil, n.OnSplit)
 		leftNode.Parent = upperNode
 		returnNode = upperNode
 
 		upperNode.Childs[0] = leftNode
 	}
 
-	rightNode := NewNode(n.Size, upperNode)
+	rightNode := NewNode(n.Size, upperNode, n.OnSplit)
 
 	pivotValue := leftNode.Data[n.Size/2]
 
 	idx := upperNode.FindIdxToInsert(pivotValue)
 	upperNode.insertAt(idx, pivotValue)
+	n.OnSplit(pivotValue, upperNode)
+
 	upperNode.Childs[idx+1] = rightNode
 
 	rightNode.Data = append(rightNode.Data, leftNode.Data[n.Size/2 + 1:]...)
+
+	for _, v := range(rightNode.Data) {
+		n.OnSplit(v, rightNode)
+	}
+
 	leftNode.Data = leftNode.Data[:n.Size/2]
+
+	for _, v := range(leftNode.Data) {
+		n.OnSplit(v, leftNode)
+	}
 
 	return returnNode
 }
 
-func (n *Node) Insert(newValue Lesser) *Node {
+func (n *Node) Insert(newValue Lesser) (*Node, *Node) {
+	insertedNode := n
+
 	if uint16(len(n.Data)) == n.Size {
 		n = n.Split()
 	}
 
 	idx := n.FindIdxToInsert(newValue)
 	if n.Childs[idx] != nil {
-		n.Childs[idx] = n.Childs[idx].Insert(newValue)
+		n.Childs[idx], insertedNode = n.Childs[idx].Insert(newValue)
 	} else {
 		n.insertAt(idx, newValue)	
 	}
 	
-	return n
+	return n, insertedNode
 }
+
+func (n *Node) FindByLocalKey(key string) (int, bool) {
+	for i, v := range n.Data {
+		if v.Key() == key {
+			return i, true
+		}
+	}
+
+	return 0, false
+}
+
+
+func (n *Node) RemoveByLocalKey(key string) bool {
+	idx, ok := n.FindByLocalKey(key)
+	if !ok {
+		return false
+	}
+
+	return n.RemoveByLocalIdx(uint16(idx))
+}
+
+func (n *Node) RemoveByLocalIdx(idx uint16) bool {
+	if idx >= n.Size {
+		return false
+	}
+
+	if n.Childs[idx+1] != nil {
+		n.Data[idx] = n.Childs[idx+1].Data[0]
+		return n.Childs[idx+1].RemoveByLocalIdx(0)
+	}
+
+	copy(n.Data[idx:], n.Data[idx+1:])
+	return true
+}
+
 
 func (n Node) FindIdxToInsert(newValue Lesser) uint16 {
 	idx := uint16(len(n.Data))
@@ -139,6 +200,20 @@ func (n *Node) stringLoopHelper() string {
 	return res
 }
 
+func (n *Node) DepthFirstTraverse(visiter func(v Lesser)) {
+	for i, v := range n.Childs {
+
+		if v != nil {
+			v.DepthFirstTraverse(visiter)
+		}
+
+		
+		if i < len(n.Data) {
+			visiter(n.Data[i])	
+		}		
+	}
+}
+
 func (n *Node) String() string {
 
 	res := fmt.Sprint(n.Data)
@@ -147,13 +222,52 @@ func (n *Node) String() string {
 	return res
 }
 
-
-
 type Store struct {
-
+	btree *Node
+	ridx  map[string]*Node
 }
 
+func NewStore() Store {
+	res := Store{}
 
-func (s *Store) Put(a int) error {
+	res.btree = NewNode(5, nil, res.onSplit)
+	res.ridx = make(map[string]*Node)
+
+	return res
+}
+
+func (s *Store) onSplit(value Lesser, n *Node) {
+	s.ridx[value.Key()] = n
+}
+
+func (s *Store) Insert(value Lesser) error {
+	n, ok := s.ridx[value.Key()]
+	if ok {
+		n.RemoveByLocalKey(value.Key())
+	}
+
+	s.btree, n = s.btree.Insert(value)
+	s.ridx[value.Key()] = n
 	return nil
+}
+
+func (s *Store) Get(key string) Lesser {
+	n, ok := s.ridx[key]
+	if !ok {
+		return nil
+	}
+
+	if idx, ok := n.FindByLocalKey(key); ok {
+		return n.Data[idx]
+	}
+
+	return nil
+}
+
+func (s *Store) DepthFirstTraverse(visiter func(v Lesser)) {
+	s.btree.DepthFirstTraverse(visiter)
+}
+
+func (s *Store) String() string {
+	return s.btree.String()
 }
--- store_test.go
+++ store_test.go
@@ -7,36 +7,52 @@ import (
 
 
 func TestInsert(t *testing.T) {
-	sut := NewNode(5, nil)
+	sut := NewStore()
 
-	insert := func(val int) {
-		sut = sut.Insert(IntLesser(val))
-		t.Logf("insert %v: %v", val, sut.String())
+	insert := func(key string, val int, log bool) {
+		/*sut, _ =*/ sut.Insert(IntLesser{KeyValue: key, Value: int64(val)})
+
+
+		data := make([]IntLesser, 0, 100)
+		visiter := func(v Lesser) {
+			data = append(data, v.(IntLesser))
+		}
+
+		if log {
+			sut.DepthFirstTraverse(visiter)
+			t.Logf("insert %v: %v", val, data)
+			//t.Logf("insert %v: %v", val, sut.String())
+		}
 	}
 
-	sut = sut.Insert(IntLesser(10))
-	sut = sut.Insert(IntLesser(2))
-	sut = sut.Insert(IntLesser(5))
-	sut = sut.Insert(IntLesser(3))
-	sut = sut.Insert(IntLesser(1))
+	insert("10", 10, false)
+	insert("2", 2, false)
+	insert("5", 5, false)
+	insert("3", 3, false)
+	insert("1", 1, false)
 	t.Log(sut.String())
 	
-	insert(20)
-	insert(21)
-	insert(18)
-	insert(19)
-	insert(17)
-	insert(6)
-	insert(7)
-	insert(8)
-	insert(9)
-	insert(4)
-	insert(11)
-	insert(12)
-	// insert(13)
-	// insert(14)
-	// insert(15)
-	// insert(16)
+	insert("20", 20, true)
+	insert("21", 21, true)
+	insert("18", 18, true)
+	insert("19", 19, true)
+	insert("17", 17, true)
+	insert("6", 6, true)
+	insert("7", 7, true)
+	insert("8", 8, true)
+	insert("9", 9, true)
+	insert("4", 4, true)
+	insert("11", 11, true)
+	insert("12", 12, true)
+	insert("13", 13, true)
+	insert("14", 14, true)
+	insert("15", 15, true)
+	insert("16", 16, true)
+
+	t.Log("---------------------------")
+
+	v := sut.Get("11")
+	t.Logf("Get 11 %v", v)
 
 	t.Fail()
 }
